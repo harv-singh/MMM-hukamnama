574b78303b5fb2469ee5221696e0fea3e0ca477f
Harveer Singh <44998024+harv-singh@users.noreply.github.com>
2022-01-30 16:44:47 +0000

    Update MMM-hukamnama.js

--- MMM-hukamnama.js
+++ MMM-hukamnama.js
@@ -1,27 +1,26 @@
 Module.register('MMM-hukamnama', {
 
     defaults: {
-            show_all:   true,
-            interval:   600000 // Every 10 mins
 
+        interval:   31200000 // Every 520 mins
     },
 
     start:  function() {
         Log.log('Starting module: ' + this.name);
-
         // Set up the local values, here we construct the request url to use
         this.loaded = false;
         this.url = 'https://api.gurbaninow.com/v2/hukamnama/today';
+        Log.log('Using API: ' + this.url)
         this.result = null;
 
-        // Trigger the first request
-        this.getHukamnamaStatusData(this);
-        },
+       // Trigger the first request        
+       this.getHukamnamaStatusData(this);
+       
+       },
 
     getStyles: function() {
         return ['hukamnama.css'];
         },
-
 
     getHukamnamaStatusData: function(that) {
         // Make the initial request to the helper then set up the timer to perform the updates
@@ -31,21 +30,44 @@ Module.register('MMM-hukamnama', {
 
 
     getDom: function () {
+        
         var wrapper = document.createElement('div');
-        wrapper.className = 'tubeStatus bright';
-
-        if (this.loaded) {
+        
+        wrapper.className = 'hukamnamastatus bright';
+        
+        var hukamnamaArray = this.result.hukamnama;
+        var hukamnamaInfo = this.result.hukamnamainfo;
+        var hDate = this.result.date;
+        
+        var hukamnamaDate = hDate.gregorian.day + ' ' + hDate.gregorian.month +
+        ' ' + hDate.gregorian.date + 'th ' + hDate.gregorian.year;
+        
+        var title = hukamnamaDate + ' ' + hukamnamaInfo.source.english
+        + ' ' + hukamnamaInfo.writer.english + ' ' + hukamnamaInfo.raag.raagwithpage;
+    
+        
+            if (this.loaded) {
             if (this.result !== null) {
                 hukamnamaResults = document.createElement('table');
-                hukamnamaResults.className = 'tubeStatus bright';
+                hukamnamaResults.className = 'hukamnamastatus bright';
+        
+                lineTop = document.createElement('tr');
+                lineTop.innerHTML = title;
+                
+                hukamnamaResults.appendChild(lineTop);
+                
+                for (var i=0; i < hukamnamaArray.length; i++) {
+                                        
+                    lineRow1 = document.createElement('tr');
+                    lineRow1.innerHTML = this.result.hukamnama[i].line.gurmukhi.unicode;
+                   
+                    lineRow2 = document.createElement('tr');
+                    lineRow2.innerHTML = this.result.hukamnama[i].line.translation.english.default;
+                   
+                    hukamnamaResults.appendChild(lineRow1); 
+                    hukamnamaResults.appendChild(lineRow2);
 
-                for (var i=0; i < this.result.length; i++) {
-                    lineRow = document.createElement('tr');
-                    lineName = document.createElement('td');
-                    lineName.innerHTML = this.result.hukamnama[i].line.translation.english.default;
-                    lineRow.appendChild(lineName);
                 }
-                hukamnamaResults.appendChild(lineRow);
             }
             wrapper.appendChild(hukamnamaResults);
         }
@@ -54,9 +76,7 @@ Module.register('MMM-hukamnama', {
 
 
     socketNotificationReceived: function(notification, payload) {
-        // check to see if the response was for us and used the same url
         if (notification === 'GOT-HUKAMNAMA' && payload.url === this.url) {
-                // we got some data so set the flag, stash the data to display then request the dom update
                 this.loaded = true;
                 this.result = payload.result;
                 this.updateDom(1000);
